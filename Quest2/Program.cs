432ec5e0e46e958bbea7a9e9d6c470b48e0a4e14
Roman_Egorov <egorovrp1991@mail.ru>
2023-07-18 20:05:13 +0300

    Initial commot

---
+++ Quest2/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+//3, 5 -> 243 (3⁵)
+//2, 4 -> 16
+
+double Promt(string message)
+{
+    Console.Write(message);
+	double num = double.Parse(Console.ReadLine());
+	return num;
+}
+
+double Step (double a, double b)
+{
+	double result = Math.Pow(a , b);
+	return result;
+}
+
+double A = Promt ("Введите число A: ");
+double B = Promt ("Введите число B: ");
+double Res = Step(A,B);
+Console.Write("Результат возведения в степень: " + Res);
---
+++ Quest3/Program.cs
@@ -0,0 +1,33 @@
+﻿//Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+
+int Promt (string message)
+{
+	Console.Write(message);
+	int num = int.Parse(Console.ReadLine()!);
+	return num;
+}
+
+int SearchNumber (int a)
+{
+int [] array = {};
+int i;
+while ();
+
+}
+
+int Number = Promt ("Введите число: ");
+
+
+
+
+// 	if(a >= 10)  //находим первое число
+// 	num1 = a/10;
+	
+// 	if(a>=100) // находим второе число
+// 	num2 = (a/10)%10;
+	
+// 	if (a>1000) // находим третье число
+// 	num3= (a/10);
